6203b7ef74f8f4b069d5bd08dbbfc52e66274040
Justinas Bolys <justinas.bolys@gmail.com>
2016-12-08 15:20:57 +0200

    Update bootstrap.php
    Actually Fabien Potencier was the author, so revert copyright.

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * (c) Justinas Bolys <justinas.bolys@gmail.com>
+ * (c) Fabien Potencier <fabien@symfony.com>, Justinas Bolys <justinas.bolys@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
